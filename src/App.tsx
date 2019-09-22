9187cc4cee9cefc12fb4f868e64d991dde4df4b8
AustinLynes <austinlynes94@gmail.com>
2019-09-21 19:19:54 -0700

    created broiler-plate for react/desktop applications

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.scss';
+
+const App = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+       <p>use this as a preset to start your desktop applications using React framework</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
